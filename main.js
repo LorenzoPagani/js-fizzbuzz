55b011b31708850964ac7e053ee6d817e5cd95ab
Lorenzo Pagani <larrypg2@gmail.com>
2023-10-30 15:40:43 +0100

    added fizz buzz in DOM

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+let container = document.querySelector(".container");
+
 for (let i = 1; i <= 100; i++) {
-  
+
+    const newparagraph = document.createElement("p");
+    
     if(i%3 == 0 && i%5 == 0)  {
+        newparagraph.innerHTML = "fizzbuzz";
+        container.append(newparagraph);
         console.log(`fizzbuzz`);
     }
 
     else if ( i%3 == 0)  {
-      
-        console.log(`fizz`);
+        newparagraph.innerHTML = "fizz";
+        container.append(newparagraph);
+        console.log(`fizz`)
     }
 
     else if(i%5 == 0) {
-        console.log(`buzz`);
+        newparagraph.innerHTML = "buzz";
+        container.append(newparagraph);
+        console.log(`buzz`)
     }
 
     else {
+        newparagraph.innerHTML = i;
+        container.append(newparagraph);
         console.log(i);
     }
 }
+
+
+
+container.innerHTML += "fine";
+console.log("fine");
