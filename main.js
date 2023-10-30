bacba7aba8f477ddb076d9657c8bd14e074874bf
Lorenzo Pagani <larrypg2@gmail.com>
2023-10-30 16:12:19 +0100

    added style to elements

--- main.js
+++ main.js
@@ -6,24 +6,28 @@ for (let i = 1; i <= 100; i++) {
     
     if(i%3 == 0 && i%5 == 0)  {
         newparagraph.innerHTML = "fizzbuzz";
+        newparagraph.classList.add("fizzbuzz");
         container.append(newparagraph);
         console.log(`fizzbuzz`);
     }
 
     else if ( i%3 == 0)  {
         newparagraph.innerHTML = "fizz";
+        newparagraph.classList.add("fizz");
         container.append(newparagraph);
         console.log(`fizz`)
     }
 
     else if(i%5 == 0) {
         newparagraph.innerHTML = "buzz";
+        newparagraph.classList.add("buzz");
         container.append(newparagraph);
         console.log(`buzz`)
     }
 
     else {
         newparagraph.innerHTML = i;
+        newparagraph.classList.add("normal");
         container.append(newparagraph);
         console.log(i);
     }
